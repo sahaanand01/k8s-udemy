25dad08bddac26baf5b2839561a020e4b8cbca65
AnandaGeethan <sahaanand00@gmail.com>
2024-06-03 09:17:59 +0530

    Created react app with docker and k8s and Docker and K8s deployment works fine

---
+++ k8s-own/react/myapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hi Running from Docker Container
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learning Continue not gonna stop
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
