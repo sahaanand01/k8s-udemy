eff7323a2c503d49bc5eb2c7ff54adc3f023ca9b
Sahaya Arulananda Selva <SahayaArulanandaS@clarium.tech>
2024-06-06 16:26:11 +0530

    Added the backend for bucket in providr.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,9 @@ provider "google" {
   project = var.project_id
   region = var.project_region
 }
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-bucket-bkup"
+    prefix  = "terraform/state"
+  }
+}
